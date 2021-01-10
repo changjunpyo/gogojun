0ef57f2ce7b95b0d2bdb83f14c974643745c09d0
JP <changjunpyo@gmail.com>
2021-01-10 21:11:07 +0900

    init gogojun

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `GoGoJun`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Jp chang`,
+      summary: `Personal blog by`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    description: `I write the post about what I learned in everyday life with words and code`,
+    social: {},
   },
   plugins: [
     {
@@ -60,7 +57,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
+        name: `GoGoJun`,
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#ffffff`,
